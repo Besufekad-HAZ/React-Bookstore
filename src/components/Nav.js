da6e9ef2384ace1adaa4e9c1c9cbb6769ba25825
Besufekad Alemu <besurye@gmail.com>
2023-04-09 01:02:49 +0300

    start styling on the bookstore app

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FaRegUserCircle } from 'react-icons/fa';
 
 const NavBar = () => (
   <nav className="Nav-parent-container">
     <h1>Bookstore CMS</h1>
     <ul className="nav-container">
       <li>
-        <Link to="/"> Books </Link>
+        <Link to="/"> BOOKS </Link>
       </li>
       <li>
-        <Link to="/categories">Categories</Link>
+        <Link to="/categories">CATEGORIES</Link>
       </li>
     </ul>
+    <div className="user_icon">
+      <FaRegUserCircle className="fa-icon" />
+    </div>
   </nav>
 );
 
